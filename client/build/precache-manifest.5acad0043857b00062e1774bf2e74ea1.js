931c7da65b48d09df585f3becfc915ef81ca8d3b
Evgene <evisotskiy@zaraffasoft.com>
2020-01-28 15:59:16 +0200

    Add build folder to the repo so that project could run on heroku

---
+++ client/build/precache-manifest.5acad0043857b00062e1774bf2e74ea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6c46b9f538030d39f7ebfce918585b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05489c8509a37ed493c9",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "22277e0ae697e9fe922e",
+    "url": "/static/css/main.9a7711b1.chunk.css"
+  },
+  {
+    "revision": "05489c8509a37ed493c9",
+    "url": "/static/js/2.50662501.chunk.js"
+  },
+  {
+    "revision": "448d43f5025c2d14258435ed861ea1a6",
+    "url": "/static/js/2.50662501.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22277e0ae697e9fe922e",
+    "url": "/static/js/main.d4a099ef.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
